ad9b6b99420898277a65190fb4844b1d612d67c6
quintene <q@qntn.nl>
2022-06-03 11:37:23 +0200

    kubernetes configurations
    Include configurations for kubernetes infrastructure

--- mongo-setup/mongo-setup.sh
+++ mongo-setup/mongo-setup.sh
@@ -1,3 +1,3 @@
-mongo mongodb://order-db:27017 --eval "rs.initiate(); rs.status()" --username root --password mongo
-mongo mongodb://stock-db:27017 --eval "rs.initiate(); rs.status()" --username root --password mongo
-mongo mongodb://payment-db:27017 --eval "rs.initiate(); rs.status()" --username root --password mongo
+mongo mongodb://order-db-service:27017 --eval "rs.initiate(); rs.status()" --username root --password mongo
+mongo mongodb://stock-db-service:27017 --eval "rs.initiate(); rs.status()" --username root --password mongo
+mongo mongodb://payment-db-service:27017 --eval "rs.initiate(); rs.status()" --username root --password mongo
