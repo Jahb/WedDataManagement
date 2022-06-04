177ade3299b4f7dc89d50e6db756bbf25d8cc82c
Jahson <jahson.binda@gmail.com>
2022-06-04 19:26:55 +0200

    edit for default config

--- deploy-charts-cluster.sh
+++ deploy-charts-cluster.sh
@@ -5,5 +5,6 @@ helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
 
 helm repo update
 
-helm install -f helm-config/redis-helm-values.yaml redis bitnami/redis
+# helm install -f helm-config/redis-helm-values.yaml redis bitnami/redis
+helm install my-mongodb bitnami/mongodb --version 12.1.15
 helm install -f helm-config/nginx-helm-values.yaml nginx ingress-nginx/ingress-nginx
--- deploy-charts-minikube.sh
+++ deploy-charts-minikube.sh
@@ -3,4 +3,5 @@
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo update
 
-helm install -f helm-config/redis-helm-values.yaml redis bitnami/redis
+# helm install -f helm-config/redis-helm-values.yaml redis bitnami/redis
+helm install my-mongodb bitnami/mongodb --version 12.1.15
