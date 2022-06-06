27ae21000ea8933b7c6f497bf3fce63ff83dfc16
Tim Elgersma <timothy.elgersma@gmail.com>
2022-06-06 09:56:06 +0200

    switched to fastapi + uvicorn

--- stock/app.py
+++ stock/app.py
@@ -1,13 +1,17 @@
+from http import HTTPStatus
 import os
 import atexit
-
-from flask import Flask
-from flask import jsonify
+from fastapi import FastAPI, HTTPException
 
 import pymongo
 from bson.objectid import ObjectId
+import logging
 
-app = Flask("stock-service")
+LOG_FORMAT = ('%(levelname) -10s %(asctime)s %(name) -30s %(funcName) '
+              '-35s %(lineno) -5d: %(message)s')
+LOGGER = logging.getLogger(__name__)
+
+app = FastAPI(title="stock-service")
 
 client: pymongo.MongoClient = pymongo.MongoClient(
     host=os.environ['MONGO_HOST'],
@@ -29,8 +33,11 @@ atexit.register(close_db_connection)
 
 
 # Create a new Item & return the ID
-@app.post('/item/create/<price>')
+@app.post('/item/create/{price}')
 def create_item(price: float):
+    # POST - adds an item and its price, and returns its ID.
+    # Output JSON fields:
+    # “item_id” - the item’s id
     item = {"stock": 0, "price": price}
     stock.insert_one(item)
     item["_id"] = str(item["_id"])
@@ -38,8 +45,12 @@ def create_item(price: float):
     return item
 
 
-@app.get('/find/<item_id>')
+@app.get('/find/{item_id}')
 def find_item(item_id: str):
+    # GET - returns an item’s availability and price.
+    # Output JSON fields:
+    # “stock” - the item’s stock
+    # “price” - the item’s price
     item = stock.find_one({"_id": ObjectId(item_id)})
     item["_id"] = str(item["_id"])
     item["item_id"] = str(item["_id"])
@@ -48,28 +59,31 @@ def find_item(item_id: str):
     return item
 
 
-@app.post('/add/<item_id>/<amount>')
+@app.post('/add/{item_id}/{amount}')
 def add_stock(item_id: str, amount: float):
-    modified_count = stock.update_one({"_id": ObjectId(item_id)}, {"$inc": {"stock": float(amount)}}).modified_count
+    # POST - adds the given number of stock items to the item count in the stock
+    modified_count = stock.update_one({"_id": ObjectId(item_id)}, {
+                                      "$inc": {"stock": float(amount)}}).modified_count
     if int(modified_count) == 1:
-        return jsonify({"success": True}), 200
-    else:
-        return jsonify({'error': 'Fail'}), 400
+        return {"done": True}
+    raise HTTPException(HTTPStatus.UNPROCESSABLE_ENTITY,
+                        "add_stock could not complete")
 
 
-@app.post('/subtract/<item_id>/<amount>')
+@app.post('/subtract/{item_id}/{amount}')
 def remove_stock(item_id: str, amount: float):
+    # POST - subtracts an item from stock by the amount specified.
+    # TODO - how will we make this idempotent?
     with client.start_session() as session:
         with session.start_transaction():
-            try:
-                item = find_item(item_id)
-                if item["stock"] < float(amount):
-                    return jsonify({'error': 'insufficient stock'}), 444
-                
-                modified_count = stock.update_one({"_id": ObjectId(item_id)}, {"$inc": {"stock": -float(amount)}}).modified_count
-                if int(modified_count) == 1:
-                    return jsonify({"success": True}), 200
-                else:
-                    return jsonify({'error': 'Fail'}), 400
-            except Exception as e:
-                return jsonify({"error" : str(e)}), 400
+            item = find_item(item_id)
+            if item["stock"] < float(amount):
+                raise HTTPException(
+                    HTTPStatus.PRECONDITION_FAILED, f"insufficient stock for item {item}")
+
+            modified_count = stock.update_one({"_id": ObjectId(item_id)}, {
+                                              "$inc": {"stock": -float(amount)}}).modified_count
+            if int(modified_count) == 1:
+                return {"done": True}
+            raise HTTPException(HTTPStatus.UNPROCESSABLE_ENTITY,
+                                "add_stock could not complete")
